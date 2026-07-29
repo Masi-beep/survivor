738786d6c055650e92f9a83d8e5b286d07ad331e
Masi-beep <maximo.lizarraga@gmail.com>
2026-07-29 17:22:03 -0300

    yup

--- code/main.py
+++ code/main.py
@@ -3,6 +3,7 @@ from settings import *
 from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 from random import randint
 
@@ -14,16 +15,34 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.running = True
         self.clock = pygame.time.Clock()
-
+        self.load_images()
         # grop
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
         
         self.setup()
 
-        # sprites
-        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
+        # gun timer
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
 
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join("..", "images", "gun", "bullet.png")).convert_alpha()
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 50
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
     
     def setup(self):
         map = load_pygame(join("..", "data", "maps", "world.tmx"))
@@ -36,6 +55,12 @@ class Game:
         for obj in map.get_layer_by_name("Objects"):
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
         
+        for obj in map.get_layer_by_name("Entities"):
+            if obj.name == "Player":
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
+
+
     def run(self):
         while self.running:
             # dt
@@ -46,12 +71,14 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            # update 
+            # update
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
 
             # draw 
             self.display_surface.fill('grey')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
         pygame.quit()
